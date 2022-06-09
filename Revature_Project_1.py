5f7b3fe6308951b1afe9445843e4c0f9140985f9
EvieMahsem <106112473+EvieMahsem@users.noreply.github.com>
2022-06-09 16:53:54 -0500

    Primary Key Table Creation done

--- Revature_Project_1.py
+++ Revature_Project_1.py
@@ -1,38 +1,6 @@
 import maskpass
 import mysql.connector
 
-
-# This is a class for the C of the CRUD operations
-# class Creation():
-#     def __init__(self, connection, database_name, table_name):
-#         self.database_name = database_name
-#         self.table_name = table_name
-
-def create_database(connection, databsae_name):
-    databaseObject = connection.cursor()
-    databaseObject.execute(f"CREATE DATABASE {databsae_name};")
-
-def use_database(connection, databsae_name):
-    useObject = connection.cursor()
-    useObject.execute(f"USE {databsae_name};")
-
-# This is a class for the R of the CRUD operations
-# class Sql_Read():
-
-
-# This is a class for the U of the CRUD operations
-# class Sql_Update():
-
-
-# This is a class for the D of the CRUD operations
-# class Sql_Delete():
-
-
-
-
-###This is used to run the code. This will be later moved to a run sutie.###
-
-
 def setup():
     # User inputted MySQL server info
     username = input("Input your MySql username: ")
@@ -46,28 +14,86 @@ def setup():
                                 host = host,
                                 port = port,
                                 passwd = passwd1)
+
     #Database selection / creation                            
     while True:
         print("Do you want to create database a yes or no?")
-        DBCreation = input("\t Type 1 for yes and Type 2 for no:\n \t Enter: ")
-        if int(DBCreation) == 1:
+        DBCreation = input("\t Type yes or no: ")
+
+        if DBCreation.lower() == "yes":
+
+            #Text prompts and writes a file with created databases
             name = input("Name your database please: ")
             databases = open("databases.txt", "a")
             databases.write(name + ",")
             databases.close()
-            create_database(conn, name)
-        elif int(DBCreation) == 2:
+
+            #Creates the database
+            databaseObject = conn.cursor()
+            databaseObject.execute(f"CREATE DATABASE {name};")
+        elif DBCreation.lower() == "no":
+
+            #Text prompts and reads the created databases
             print("Which database do you want to use: ")
             databases = open("databases.txt", "r")
             print("Created databases:", databases.read())
             databases.close()
             name = input("The database you want: ")
-            use_database(conn, name)
+
+            #Uses the database selected
+            useObject = conn.cursor()
+            useObject.execute(f"USE {name};")
+            
+            #Stops the while loop
             break
 
-setup()
+def table_creation():
+    while True:
+        yes_no = input("Are you wanting to create a table(yes/no): ")
+        if yes_no.lower() == "yes":
+            #Text prompts and writes a file with created databases
+            table_name = input("Name your table please: ")
+            tables = open("tables.txt", "a")
+            tables.write(table_name + ",")
+            tables.close()
+
+            p_f_d = input("What type of key do you want?\n \t 1. Primary Key\n \t 2. Foreign Key\n \t 3. No Key (default) \n \t Response: ")
+            num_of_col = input("How many columns do you want: ")
+            if int(p_f_d) == 1:
+                col_datatype = []
+                for i in range(int(num_of_col)):
+                    x = input("What is column name and datatype (name datatype)?\nLink to datatypes: https://www.w3schools.com/mysql/mysql_datatypes.asp \n \t Your input please: ")
+                    col_datatype.append(x)
+                string_of_col = ",".join(col_datatype)
+                print(string_of_col)
+                key = input("Which column should be the primary key: ")
+                tableObject = conn.cursor()
+                tableObject.execute(f"CREATE TABLE {table_name} ({string_of_col}, PRIMARY KEY({key}));")
+
+            elif int(p_f_d) == 2:
+                pass
+            else:
+                pass
+
+
+
+
+
+        else:
+            break
+
 
 
 def shutdown():
     conn.close()
+
+
+#Runs all of the functions
+setup()
+table_creation()
+
+
+
 shutdown()
+
+
