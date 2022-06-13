0fd82bcdf683cf2b77eaaa9b14ab416e7733f7c7
EvieMahsem <106112473+EvieMahsem@users.noreply.github.com>
2022-06-12 21:01:10 -0500

    Added a way to insert data
    For insert data I only added a way to insert data one at a time. I have yet to get the randomize data. I also made some other changes that made sense

--- Revature_Project_1.py
+++ Revature_Project_1.py
@@ -15,119 +15,139 @@ def setup():
                                 port = port,
                                 passwd = passwd1)
 
-def database_selection_creation():
+def database_creation():
     #Database selection / creation                            
     while True:
-        print("Do you want to create database a yes or no?")
-        DBCreation = input("\t Type yes or no: ")
+        #Text prompts and writes a file with created databases
+        name = input("Name your database please: ")
+        databases = open("databases.txt", "a")
+        databases.write(name + "\n")
+        databases.close()
 
-        if DBCreation.lower() == "yes":
+        #Creates the database
+        databaseObject = conn.cursor()
+        databaseObject.execute(f"CREATE DATABASE {name};")
 
-            #Text prompts and writes a file with created databases
-            name = input("Name your database please: ")
-            databases = open("databases.txt", "a")
-            databases.write(name + ",")
-            databases.close()
+        break
 
-            #Creates the database
-            databaseObject = conn.cursor()
-            databaseObject.execute(f"CREATE DATABASE {name};")
-        elif DBCreation.lower() == "no":
+def database_selection():
+    #Text prompts and reads the created databases
+    print("Which database do you want to use: ")
+    databases = open("databases.txt", "r")
+    print("Created databases:\n" + databases.read())
+    databases.close()
+    name = input("The database you want: ")
 
-            #Text prompts and reads the created databases
-            print("Which database do you want to use: ")
-            databases = open("databases.txt", "r")
-            print("Created databases:", databases.read())
-            databases.close()
-            name = input("The database you want: ")
+    #Uses the database selected
+    useObject = conn.cursor()
+    useObject.execute(f"USE {name};")
 
-            #Uses the database selected
-            useObject = conn.cursor()
-            useObject.execute(f"USE {name};")
-            
-            #Stops the while loop
-            break
 
 def table_creation():
     while True:
-        yes_no = input("Are you wanting to create a table(yes/no): ")
-        if yes_no.lower() == "yes":
-            #Text prompts and writes a file with created tables
-            table_name = input("Name your table please: ")
-            tables = open("tables.txt", "a")
-            tables.write(table_name + ",")
-            tables.close()
+        #Text prompts and writes a file with created tables
+        table_name = input("Name your table please: ")
+        tables = open("tables.txt", "a")
+        tables.write(table_name + "\n")
+        tables.close()
 
-            #Text prompt for what type of table to make
-            p_f_d = input("What type of key do you want?\n \t 1. Primary Key\n \t 2. Foreign Key\n \t 3. No Key (default) \n \t Response: ")
-            num_of_col = input("How many columns do you want: ")
+        #Text prompt for what type of table to make
+        p_f_d = input("What type of key do you want?\n \t 1. Primary Key\n \t 2. Foreign Key\n \t 3. No Key (default) \n \t Response: ")
+        num_of_col = input("How many columns do you want: ")
 
-            if int(p_f_d) == 1:
+        if int(p_f_d) == 1:
 
-                #Takes how many columns you want and will let you insert column_name and datatype pairs and then formats it into a 1 string
-                col_datatype = []
-                for i in range(int(num_of_col)):
-                    x = input("What is column name and datatype (column_name datatype)?\nLink to datatypes: https://www.w3schools.com/mysql/mysql_datatypes.asp \n \t Your input please: ")
-                    col_datatype.append(x)
-                string_of_col = ",".join(col_datatype)
+            #Takes how many columns you want and will let you insert column_name and datatype pairs and then formats it into a 1 string
+            col_datatype = []
+            for i in range(int(num_of_col)):
+                x = input("What is column name and datatype (column_name datatype)?\nLink to datatypes: https://www.w3schools.com/mysql/mysql_datatypes.asp \n \t Your input please: ")
+                col_datatype.append(x)
+            string_of_col = ",".join(col_datatype)
 
-                #Tells you the current columns and asks for which column should have the primary key
-                print("The tables current columns: ", string_of_col)
-                key = input("Which column should be the primary key: ")
+            #Tells you the current columns and asks for which column should have the primary key
+            print("The tables current columns: ", string_of_col)
+            key = input("Which column should be the primary key: ")
 
-                #Runs the SQL code
-                tableObject = conn.cursor()
-                tableObject.execute(f"CREATE TABLE {table_name} ({string_of_col}, PRIMARY KEY ({key}));")
+            #Runs the SQL code
+            tableObject = conn.cursor()
+            tableObject.execute(f"CREATE TABLE {table_name} ({string_of_col}, PRIMARY KEY ({key}));")
+        elif int(p_f_d) == 2:
 
-            elif int(p_f_d) == 2:
+            #Takes how many columns you want and will let you insert column_name and datatype pairs and then formats it into a 1 string
+            col_datatype = []
+            for i in range(int(num_of_col)):
+                x = input("What is column name and datatype (column_name datatype)?\nLink to datatypes: https://www.w3schools.com/mysql/mysql_datatypes.asp \n \t Your input please: ")
+                col_datatype.append(x)
+            string_of_col = ",".join(col_datatype)
 
-                #Takes how many columns you want and will let you insert column_name and datatype pairs and then formats it into a 1 string
-                col_datatype = []
-                for i in range(int(num_of_col)):
-                    x = input("What is column name and datatype (column_name datatype)?\nLink to datatypes: https://www.w3schools.com/mysql/mysql_datatypes.asp \n \t Your input please: ")
-                    col_datatype.append(x)
-                string_of_col = ",".join(col_datatype)
+            #Tells you the current columns
+            print("The tables current columns: ", string_of_col)
 
-                #Tells you the current columns
-                print("The tables current columns: ", string_of_col)
+            #Gives the made tables, so you can select which one you want for the foreign key
+            current_table = open("tables.txt", "r")
+            print("Created tables:\n " + current_table.read())
+            current_table.close()
 
-                #Gives the made tables, so you can select which one you want for the foreign key
-                current_table = open("tables.txt", "r")
-                print("Created tables: ", current_table.read())
-                current_table.close()
+            #Gets the reference table and comlumn
+            key_table = input("Which table does the foreign key reference: ")
+            key = input("Which column should the foreign key reference: ")
 
-                #Gets the reference table and comlumn
-                key_table = input("Which table does the foreign key reference: ")
-                key = input("Which column should the foreign key reference: ")
+            #Runs the SQL code
+            tableObject = conn.cursor()
+            tableObject.execute(f"CREATE TABLE {table_name} ({string_of_col}, FOREIGN KEY ({key}) REFERENCES {key_table}({key}));")
 
-                #Runs the SQL code
-                tableObject = conn.cursor()
-                tableObject.execute(f"CREATE TABLE {table_name} ({string_of_col}, FOREIGN KEY ({key}) REFERENCES {key_table}({key}));")
-            else:
-                #Takes how many columns you want and will let you insert column_name and datatype pairs and then formats it into a 1 string
-                col_datatype = []
-                for i in range(int(num_of_col)):
-                    x = input("What is column name and datatype (column_name datatype)?\nLink to datatypes: https://www.w3schools.com/mysql/mysql_datatypes.asp \n \t Your input please: ")
-                    col_datatype.append(x)
-                string_of_col = ",".join(col_datatype)
+        elif int(p_f_d) == 3:
+            #Takes how many columns you want and will let you insert column_name and datatype pairs and then formats it into a 1 string
+            col_datatype = []
+            for i in range(int(num_of_col)):
+                x = input("What is column name and datatype (column_name datatype)?\nLink to datatypes: https://www.w3schools.com/mysql/mysql_datatypes.asp \n \t Your input please: ")
+                col_datatype.append(x)
+            string_of_col = ",".join(col_datatype)
 
-                #Tells you the current columns
-                print("The tables current columns: ", string_of_col)
+            #Tells you the current columns
+            print("The tables current columns: ", string_of_col)
 
-                #Runs the SQL code
-                tableObject = conn.cursor()
-                tableObject.execute(f"CREATE TABLE {table_name} ({string_of_col});")
-
+            #Runs the SQL code
+            tableObject = conn.cursor()
+            tableObject.execute(f"CREATE TABLE {table_name} ({string_of_col});")
         else:
-            break
+            print("Please insert a vaild value.")
+        break
 
 def table_insertion():
     while True:
-        question = input("Would you like to insert into a table or make a change:\n \t 1. Insert data\n \t 2. Exit\n \t Enter a Number: ")
+        question = input("""Welcome to data insertion:
+    1. Insert data
+    2. Insert data with random values (Not Implemented)
+    3. Exit
+    Enter a Number: """)
         if int(question) == 1:
-            pass
+
+            print("Warning: You must input data into a primary key table, before you can insert into a foreign key table!!!!\n")
+            #Shows the current tables
+            tables = open("tables.txt", "r")
+            print("Created Tables:\n" + tables.read())
+            tables.close()
+            #Gets the columns and table name
+            table_name = input("What table do you want to insert into: ")
+            coln = input("What are the column you would like to input (column1, column2, column3,...): ")
+
+
+            num_of_row = input("How many rows do you want to input: ")
+            x = 0
+            while x < int(num_of_row):
+                x += 1
+                values = input("What are the values you would like to input (val1, 'val2', 'val3',...): ")
+
+                #Runs the SQL code
+                insertObject = conn.cursor()
+                insertObject.execute(f"INSERT INTO {table_name} ({coln}) VALUES ({values});")
+                conn.commit()    
+
         elif int(question) == 2:
-            break
+            pass
+        elif int(question) == 3:
+            break        
         else:
             print("Please input a vaild value.")
 
--- run_suite.py
+++ run_suite.py
@@ -1,34 +1,38 @@
 from Revature_Project_1 import *
 
-print("Thank you from using Evie's Database Mangament System for all of your store's needs!")
+print("Thank you from using Evie's DataBase Mangament System for all of your store's needs!")
 while True:
-    print("What would you like to do?")
-    print("\t1. Setup (Required)")
-    print("\t2. Databases Creation and Selection")
-    print("\t3. Table Creation")
-    print("\t4. Insert Data (Not Implemented)")
-    print("\t5. Alter Data (Not Implemented)")
-    print("\t6. Delete Tables/Databases (Not Implemented)")
-    print("\t7. Show Tables (Not Implemented)")
-    print("\t8. Shutdown")
-    
+    print("""
+    \nWhat would you like to do?
+    \t1. Setup (Required)
+    \t2. Databases Creation
+    \t3. Databases Selection (Required)
+    \t4. Table Creation
+    \t5. Insert Data (Somewhat Implemented)
+    \t6. Alter Data (Not Implemented)
+    \t7. Delete Tables/Databases (Not Implemented)
+    \t8. Show Tables (Not Implemented)
+    \t9. Shutdown
+    """)
     x = input("What is your selection: ")
-
     if int(x) == 1:
         setup()
     elif int(x) == 2:
-        database_selection_creation()
+        database_creation()
     elif int(x) == 3:
-        table_creation()
+        database_selection()
     elif int(x) == 4:
-        table_insertion()
+        table_creation()
     elif int(x) == 5:
-        table_alteration()
+        table_insertion()
     elif int(x) == 6:
-        delete_data()
+        table_alteration()
     elif int(x) == 7:
-        show_tables()
+        delete_data()
     elif int(x) == 8:
+        show_tables()
+    elif int(x) == 9:
         shutdown()
         break
-        
+    else:
+        print(f"The value {x} is not a vaild input.\n")
