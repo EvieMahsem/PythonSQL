1bed89098014683d8954e22b76262c3d812ee14e
EvieMahsem <106112473+EvieMahsem@users.noreply.github.com>
2022-06-17 08:43:55 -0500

    Added Try-Except-Else statements
    Added Try-Except-Else statements on all SQL statements

--- Revature_Project_1.py
+++ Revature_Project_1.py
@@ -5,10 +5,6 @@ import random
 from pyparsing import col
 from tabulate import tabulate
 
-
-
-
-
 def delete_from_text(file, input):
     with open(f"{file}", "r") as f:
         lines = f.readlines()
@@ -34,15 +30,20 @@ def setup():
 def database_creation():
     #Database creation                            
     while True:
-        #Text prompts and writes a file with created databases
+        #Text prompts with the name of your database
         name = input("Name your database please: ")
-        databases = open("databases.txt", "a")
-        databases.write(name + "\n")
-        databases.close()
 
         #Creates the database
-        databaseObject = conn.cursor()
-        databaseObject.execute(f"CREATE DATABASE {name};")
+        try:
+            databaseObject = conn.cursor()
+            databaseObject.execute(f"CREATE DATABASE {name};")
+        except:
+            print("Please input a name that does not already exist")
+        else:
+            #If there is no error, it writes the name of the database to the txt file
+            databases = open("databases.txt", "a")
+            databases.write(name + "\n")
+            databases.close()
 
         break
 
@@ -57,8 +58,11 @@ def database_selection():
     database_selected = name
 
     #Uses the database selected
-    useObject = conn.cursor()
-    useObject.execute(f"USE {name};")
+    try:
+        useObject = conn.cursor()
+        useObject.execute(f"USE {name};")
+    except:
+        print("Please select a database that exists.")
 
 def table_creation():
     while True:
@@ -79,18 +83,21 @@ def table_creation():
                 col_datatype.append(x)
             string_of_col = ",".join(col_datatype)
 
-
-            tables_col = open("tables.txt", "a")
-            tables_col.write(table_name + f":{database_selected}:PK:" + string_of_col + "\n")
-            tables_col.close()
-
             #Tells you the current columns and asks for which column should have the primary key
             print("The tables current columns: ", string_of_col)
             key = input("Which column should be the primary key: ")
 
             #Runs the SQL code
-            tableObject = conn.cursor()
-            tableObject.execute(f"CREATE TABLE {table_name} ({string_of_col}, PRIMARY KEY ({key}));")
+            try:
+                tableObject = conn.cursor()
+                tableObject.execute(f"CREATE TABLE {table_name} ({string_of_col}, PRIMARY KEY ({key}));")
+            except:
+                print("Most likly an SQL error, so make sure you typed everything in correctly")
+            else:
+                tables_col = open("tables.txt", "a")
+                tables_col.write(table_name + f":{database_selected}:PK:" + string_of_col + "\n")
+                tables_col.close()
+
         elif int(p_f_d) == 2:
 
             #Takes how many columns you want and will let you insert column_name and datatype pairs and then formats it into a 1 string
@@ -112,14 +119,19 @@ def table_creation():
             key_table = input("Which table does the foreign key reference: ")
             key = input("Which column should the foreign key reference: ")
 
-            tables_col = open("tables.txt", "a")
-            tables_col.write(table_name + f":{database_selected}:FK({key}):" + string_of_col + "\n")
-            tables_col.close()
 
 
             #Runs the SQL code
-            tableObject = conn.cursor()
-            tableObject.execute(f"CREATE TABLE {table_name} ({string_of_col}, FOREIGN KEY ({key}) REFERENCES {key_table}({key}));")
+            try:
+                tableObject = conn.cursor()
+                tableObject.execute(f"CREATE TABLE {table_name} ({string_of_col}, FOREIGN KEY ({key}) REFERENCES {key_table}({key}));")
+            except:
+                print("Most likly an SQL error, so make sure you typed everything in correctly")
+            else:  
+                tables_col = open("tables.txt", "a")
+                tables_col.write(table_name + f":{database_selected}:FK({key}):" + string_of_col + "\n")
+                tables_col.close()
+
 
         elif int(p_f_d) == 3:
             #Takes how many columns you want and will let you insert column_name and datatype pairs and then formats it into a 1 string
@@ -132,13 +144,16 @@ def table_creation():
             #Tells you the current columns
             print("The tables current columns: ", string_of_col)
 
-            tables_col = open("tables.txt", "a")
-            tables_col.write(table_name + f":{database_selected}:NO_Key:" + string_of_col + "\n")
-            tables_col.close()
-
             #Runs the SQL code
-            tableObject = conn.cursor()
-            tableObject.execute(f"CREATE TABLE {table_name} ({string_of_col});")
+            try:
+                tableObject = conn.cursor()
+                tableObject.execute(f"CREATE TABLE {table_name} ({string_of_col});")
+            except:
+                print("Most likly an SQL error, so make sure you typed everything in correctly")
+            else:
+                tables_col = open("tables.txt", "a")
+                tables_col.write(table_name + f":{database_selected}:NO_Key:" + string_of_col + "\n")
+                tables_col.close()       
         else:
             print("Please insert a vaild value.")
         break
@@ -169,9 +184,13 @@ def table_insertion():
                 values = input("What are the values you would like to input (val1, 'val2', 'val3',...): ")
 
                 #Runs the SQL code
-                insertObject = conn.cursor()
-                insertObject.execute(f"INSERT INTO {table_name} ({coln}) VALUES ({values});")
-                conn.commit()    
+                try:
+                    insertObject = conn.cursor()
+                    insertObject.execute(f"INSERT INTO {table_name} ({coln}) VALUES ({values});")
+                    conn.commit()
+                except:
+                    print("Most likly an SQL error, so make sure you typed everything in correctly")
+                    break
 
         elif int(question) == 2:
             #Hard coded data for random
@@ -199,7 +218,7 @@ def table_insertion():
                 #values = [str(x), f'"{names.get_first_name()}"',f'"{names.get_last_name()}"', f'"{random.choice(states)}"']
                 # values = [str(x), f'"{random.choice(clothing)}"',f'"{random.choice(colors)}"']
                 values = [str(x), f'"{random.choice(online_or_instore)}"']
-                
+
                 val = ','.join(values)
                 print(val)
                 #Runs the SQL code
@@ -227,10 +246,14 @@ def data_update():
         new_val = input("What is the new value you would like to enter? ")
         column_identifier = input("Which column is the Primary Key or you want to use as an column identifier? ")
         value_identifier = input("What is the value you are using as an value identifier? ")
-
-        updateObject = conn.cursor()
-        updateObject.execute(f"UPDATE {table_to_update} SET {column_changing} = {new_val} WHERE {column_identifier} = {value_identifier};")
-        conn.commit()
+        
+        try:
+            updateObject = conn.cursor()
+            updateObject.execute(f"UPDATE {table_to_update} SET {column_changing} = {new_val} WHERE {column_identifier} = {value_identifier};")
+            conn.commit()
+        except:
+            print("Most likly an SQL error, so make sure you typed everything in correctly")
+            break
 
 def delete_data():
     while True:
@@ -247,11 +270,15 @@ def delete_data():
             tables.close()
 
             table_to_del = input("Input the table_name that you want to delete:\n ")
-            dropObject = conn.cursor()
-            dropObject.execute(f"DROP TABLE {table_to_del};")
-            
-            path_to_del = input("\nInput the full path to del i.e(table_name:database:KeyType:Column_Name DataType, ...): ")
-            delete_from_text("tables.txt", path_to_del)
+
+            try:
+                dropObject = conn.cursor()
+                dropObject.execute(f"DROP TABLE {table_to_del};")
+                
+                path_to_del = input("\nInput the full path to del i.e(table_name:database:KeyType:Column_Name DataType, ...): ")
+                delete_from_text("tables.txt", path_to_del)
+            except:
+                print("Most likly an SQL error, so make sure you typed everything in correctly")
 
         elif int(x)==2:
             
@@ -261,8 +288,12 @@ def delete_data():
             tables.close()
 
             data_to_del = input("\nInput the table_name that you want to truncate: ")
-            truncateObject = conn.cursor()
-            truncateObject.execute(f"TRUNCATE TABLE {data_to_del};")
+
+            try:
+                truncateObject = conn.cursor()
+                truncateObject.execute(f"TRUNCATE TABLE {data_to_del};")
+            except:
+                print("Most likly an SQL error, so make sure you typed everything in correctly")                
 
         elif int(x) == 3:
 
@@ -271,11 +302,14 @@ def delete_data():
             print("Created databases:\n" + databases.read())
             databases.close()
 
-            database_to_del = input("Input the database name: ")
-            dropObject = conn.cursor()
-            dropObject.execute(f"DROP DATABASE {database_to_del};")
+            try:
+                database_to_del = input("Input the database name: ")
+                dropObject = conn.cursor()
+                dropObject.execute(f"DROP DATABASE {database_to_del};")
 
-            delete_from_text("databases.txt", database_to_del)
+                delete_from_text("databases.txt", database_to_del)
+            except:
+                print("Most likly an SQL error, so make sure you typed everything in correctly")
 
         elif int(x) == 4:
             break
@@ -291,12 +325,15 @@ def show_tables():
     x = input("Please input the table_name you want to see: ")
     y = input("Please, input the columns you want to see in coln1, coln2, ... format otherwise to see all use *: ")
 
-    showObject = conn.cursor()
-    showObject.execute(f"SELECT {y} FROM {x};")
-    query_result = showObject.fetchall()
+    try:
+        showObject = conn.cursor()
+        showObject.execute(f"SELECT {y} FROM {x};")
+        query_result = showObject.fetchall()
+        print(tabulate(query_result))
+    except:
+        print("Make sure you typed everything in correctly and are in the right database!")
 
-    print(tabulate(query_result))
 
 def shutdown():
     conn.close()
-
+    
